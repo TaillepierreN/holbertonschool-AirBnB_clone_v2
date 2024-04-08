c5c44d3c63863f821b5a39311a7bf919fbd9f467
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-08 15:06:57 +0200

    (web flask) refactor 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-from flask import Flask
 """ Start a Flask web app """
+
+from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def index():
     """display "Hello HBNB!" """
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
