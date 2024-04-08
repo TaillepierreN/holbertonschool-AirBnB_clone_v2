47d75238876fccd145dabdf5ea801d349c99d39a
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-08 14:26:35 +0200

    (web_flask) Renamed 2-c_route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+from flask import Flask
+''' Start a Flask web app'''
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''display "Hello HBNB!" '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''display "HBNB" '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    '''display C followed by the value of the text variable'''
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
