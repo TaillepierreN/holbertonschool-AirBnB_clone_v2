9a184fe17ab6011437397eba70a0c535b956c2aa
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-09 14:47:14 +0200

    (web_flask) added 10-hbnb_filters html py

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Start a Flask web app"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown(exception):
+    """close the storage after each requests"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """return list of states and amenities"""
+    amenities = storage.all(Amenity)
+    states = storage.all(State)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
