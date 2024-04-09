7b88038b149c531567ac5bb3f64636bd2cc44f5e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-09 11:17:25 +0200

    (web_flask) added 7-states_list py and html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Start a Flask web app"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown(exception=None):
+    """close the storage after each requests"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a list of states"""
+    list_of_states = storage.all(State).values()
+    return render_template('7-states_list.html', list_of_states=list_of_states)
+
+
+if __name__ == '__main__':
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
