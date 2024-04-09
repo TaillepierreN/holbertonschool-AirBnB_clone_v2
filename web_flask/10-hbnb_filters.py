6b0c72b3c3d25f291130a643aeaf63f475ed59e7
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-04-09 15:30:53 +0200

    (web_flask) 10 revert sorting added a fake icon.png

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -21,14 +21,6 @@ def filters():
     amenities = storage.all(Amenity).values()
     states = storage.all(State).values()
 
-    amenities = sorted(amenities, key=lambda x: x.name)
-    states = sorted(states, key=lambda x: x.name)
-
-    for state in states:
-        if storage.__class__.__name__ == 'DBStorage':
-            state.cities = sorted(state.cities, key=lambda x: x.name)
-        else:
-            state.cities = sorted(state.cities(), key=lambda x: x.name)
     return render_template('10-hbnb_filters.html',
                            states=states, amenities=amenities)
 
